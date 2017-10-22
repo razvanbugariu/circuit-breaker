1b074492a4e41ad878afd57eddc049a167550194
Razvan Bugariu <razvan.bugariu@dialogdata.de>
2017-10-22 17:05:20 +0300

    init

---
+++ src/main/java/com/rbu/demo/circuit/breaker/CircuitBreakerApplication.java
@@ -0,0 +1,12 @@
+package com.rbu.demo.circuit.breaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerApplication.class, args);
+	}
+}
